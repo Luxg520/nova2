76f8075238218e717d5f749a0001360351ae9976
qiucw <doitmyway@qq.com>
2016-12-29 21:49:07 +0800

    +UIBase UIEnum UIManager

---
+++ ClientCs/Assets/Scripts/Logic/UI/UIBase.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIBase : MonoBehaviour
+{
+    // UI信息    
+    public UIInfo UIInfo;
+
+
+    // 隐藏该界面
+    public void Hide()
+    {
+        OnHide();
+        gameObject.SetActive(false);
+    }
+
+
+    /// <summary>
+    /// 仅首次显示前调用
+    /// 优先级高于Init
+    /// </summary>
+    protected virtual void Awake()
+    {
+
+    }
+    protected virtual void Start()
+    {
+
+    }
+
+    /// <summary>
+    /// 界面初始化阶段（主要用于界面数据处理）
+    /// 每次开启界面都会调用一次
+    /// 不要在这里面播放UI动画相关操作
+    /// </summary>
+    public virtual void OnInit(params object[] _params)
+    {
+
+    }
+
+    /// <summary>
+    /// 界面显示阶段（相当于Start）
+    /// 可用于播放动画
+    /// </summary>
+    protected virtual void OnShow()
+    {
+
+    }
+
+    /// <summary>
+    /// 界面隐藏前调用
+    /// </summary>
+    protected virtual void OnHide()
+    {
+
+    }
+
+    /// <summary>
+    /// 每帧执行函数
+    /// 尽量不用，非必要时才用
+    /// </summary>
+    protected virtual void OnUpdate(float _deltaTime)
+    {
+
+    }
+
+    /// <summary>
+    /// 当界面被卸载时
+    /// </summary>
+    protected virtual void OnDestroy()
+    {
+
+    }
+
+
+    #region 内部函数
+
+    /// <summary>
+    /// 每帧运行（内部函数）
+    /// </summary>
+    void Update()
+    {
+        OnUpdate(Time.deltaTime);
+    }
+
+    #endregion
+}
---
+++ ClientCs/Assets/Scripts/Logic/UI/UIEnum.cs
@@ -0,0 +1,63 @@
+﻿/// <summary>
+/// UI类型，UI的唯一标识
+/// </summary>
+public enum UIType
+{
+    LoginUI,        // 登录
+    ServerList,     // 服务器列表
+    Loading,        // Loading
+    AccountUI,      // 账号登录注册
+    Dialog,         // 对话框
+}
+
+
+/// <summary>
+/// UI样式
+/// 普通、全屏、模态等
+/// </summary>
+public enum UIStyle
+{
+    Simple, // 普通
+    Full,   // 全屏
+    Modal,  // 模态
+}
+
+/// <summary>
+/// UI层级
+/// </summary>
+public enum UILayer
+{
+    Game,   // 游戏UI层
+    Prepose,// 游戏前置层
+    Guide,  // 游戏指引层
+    Test,   // 游戏测试层
+}
+
+/// <summary>
+/// UI信息
+/// </summary>
+public class UIInfo
+{
+    public UIType type;     // UI类型
+    public UIStyle style;   // UI样式
+    public UILayer layer;   // UI层级
+    public string resName;  // 资源名
+    public UIBase ui;       // UI基层实例
+}
+
+// UI显示隐藏动画
+public enum UIAni
+{
+    None,
+}
+
+// UI状态
+public enum UIState
+{
+    None,
+    Initial,
+    Loading,
+    Ready,
+    Disabled,
+    Closing
+}
