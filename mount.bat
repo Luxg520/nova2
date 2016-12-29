af506bf3f11523aeecaa08bd52f86c2e3b60b29f
qiucw <qiucw@g-bits.com>
2016-12-29 09:30:32 +0800

    rename

---
+++ mount.bat
@@ -0,0 +1,18 @@
+junction ClientCs\Assets\Scripts\Framework_Common Common\Framework_Common
+junction ClientCs\Assets\Scripts\Logic_Common Common\Logic_Common
+
+junction ClientJs\Assets\Scripts\Framework_Common Common\Framework_Common
+junction ClientJs\Assets\Scripts\Logic_Common Common\Logic_Common
+
+junction ClientJs\Assets\Scripts\Framework ClientCs\Assets\Scripts\Framework
+
+junction ClientJs\Assets\GameIcon ClientCs\Assets\GameIcon
+junction ClientJs\Assets\Scenes ClientCs\Assets\Scenes
+junction ClientJs\Assets\AssetBundles ClientCs\Assets\AssetBundles
+
+junction BridgeProj\Logic ClientCs\Assets\Scripts\Logic
+junction BridgeProj\Logic_Common Common\Logic_Common
+
+junction Server\Server\Common Common
+
+pause
---
+++ unmount.bat
@@ -0,0 +1,18 @@
+junction -d ClientCs\Assets\Scripts\Framework_Common
+junction -d ClientCs\Assets\Scripts\Logic_Common
+
+junction -d ClientJs\Assets\Scripts\Framework_Common
+junction -d ClientJs\Assets\Scripts\Logic_Common
+
+junction -d ClientJs\Assets\Scripts\Framework
+
+junction -d ClientJs\Assets\GameIcon
+junction -d ClientJs\Assets\Scenes
+junction -d ClientJs\Assets\AssetBundles
+
+junction -d BridgeProj\Logic
+junction -d BridgeProj\Logic_Common
+
+junction -d Server\Server\Common
+
+pause
